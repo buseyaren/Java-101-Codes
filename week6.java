f66d301b46f1c5394c932d7ec35d08ffcd8721c2
Buse Yaren Tekin <tekinbuseyaren@gmail.com>
2022-10-24 15:41:03 +0300

    Create week6.java

---
+++ week6.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+public class Main{
+
+     public static void main(String []args){
+        //int a=6;
+        //int x=5;
+        //int y=6;
+        char character = 'M';
+        char c2 = 'E';
+        double doubName = 3.14515d;
+        System.out.println(doubName);
+        System.out.println(character + ""+ c2);
+        int a=6, x=5, y=6;
+        System.out.println("x+y:"+ (x+y));  
+        String name= "Hanifi";
+        String lastname = "Ozcan";
+        //String fullname = name + " "+lastname;
+        //System.out.println("Full name: " + fullname);
+        System.out.println("Full name: " + (name+" " +lastname));
+        int myNumber = 15;
+        float testNote = 9.75f;
+        System.out.print(testNote);
+        boolean boolName = true;
+       System.out.println(boolName);
+        System.out.println(testNote);
+        System.out.println("Mynumber:"+myNumber);
+        System.out.println("Name: " +name);
+        System.out.println("Value of a:"+ " " +a);
+     }
+}
